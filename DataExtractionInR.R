23a476abe30326deb61c2545561a7d997652ebdd
abhinav choudhary <55540924+Abhi141188@users.noreply.github.com>
2021-06-22 10:03:38 +0530

    Prgrm-6

--- DataExtractionInR.R
+++ DataExtractionInR.R
@@ -8,34 +8,39 @@
 directory <-getwd()
 directory
 
-mtcars  #inbuilt datasets
+setwd("F:/WORK/rWork/rProjects/AbhiAnalytics/R-Lang-32 hrs")
+getwd()
+
+?mtcars  #inbuilt datasets
+
+#To view all the build-in dadasets in R:
 data()  #other datasets
 
 # Writing the dataset----
 #Method 1:
-write.csv(mtcars, "mtcars2.csv")
+View(mtcars)
+write.csv(mtcars, "SIT_IIM.csv")
 
 #Method 2:
 #make sure you have a folder : "data" in your project directory
 #like c1, c2, c3, c4, c5
-write.csv(x=mtcars, file='data/mtcars1.csv')
-write.csv(x=mtcars, file='data/mtcars2.csv', row.names = F)
-#save it in "data" folder .
+View(mtcars)
+write.csv(mtcars, "F:/WORK/rWork/SIT_IIM_2.csv")
+write.csv(mtcars,'XIMB.csv', row.names = F)
 
 #--------------Read the dataset------------------------
 # Method 1:
+
 f<- file.choose()
 c1 <- read.csv(f)
-
+c1
+View(c1)
 # Method 2:
-c_1 <- read.csv("data/mtcars1.csv")
+getwd()
+c_1 <- read.csv("XIMB_1.csv")
 View(c_1)
 
-newmtcars1 = read.csv(file='data/mtcars2.csv')
+newmtcars1 = read.csv('data/mtcars2.csv')
 head(newmtcars1)
 #no car names
 
-#webCSV---------
-url1 = "https://raw.githubusercontent.com/DUanalytics/BARtaxila/master/data/taxilaUsers.csv"
-taxila = read.csv(file=url1)
-head(taxila)
